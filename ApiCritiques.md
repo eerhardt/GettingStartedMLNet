7109ce3a6c8ab5da445fd248694687d62c0ec002
Eric Erhardt <eric.erhardt@microsoft.com>
2018-12-03 18:16:00 -0600

    Baseline application

---
+++ ApiCritiques.md
@@ -0,0 +1,51 @@
+﻿Here are the critiques of the current ML.NET APIs:
+
+# 1. Reading from a text file
+```C#
+    IDataView trainingData = mlContext.Data.ReadFromTextFile(
+        new[]
+        {
+            new TextLoader.Column("SepalLength", DataKind.R4, 0),
+            new TextLoader.Column("SepalWidth", DataKind.R4, 1),
+            new TextLoader.Column("PetalLength", DataKind.R4, 2),
+            new TextLoader.Column("PetalWidth", DataKind.R4, 3),
+            new TextLoader.Column("Label", DataKind.Text, 4),
+        },
+```
+
+This should just be generated from the `IrisData` class. [Tracking issue](https://github.com/dotnet/machinelearning/issues/561).
+
+# 2. Using Factory Methods over Constructors
+
+https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/constructor
+
+✓ CONSIDER providing simple, ideally default, constructors.
+
+# 3. MakePredictionFunction requires passing `mlContext`
+
+```C#
+    IrisPrediction prediction = model
+        .MakePredictionFunction<IrisData, IrisPrediction>(mlContext)
+        .Predict(newInput);
+```
+
+Potentially we could also have an overload that is just directly "Predict". Then we would have:
+
+```C#
+    IrisPrediction prediction = model.Predict<IrisData, IrisPrediction>(newInput);
+```
+
+The next step would be just to have a `Train` method on the estimator that directly returned something that can create predictions.
+
+```C#
+    var model = estimator.Train<IrisData, IrisPrediction>(trainingData);
+    IrisPrediction prediction = model.Predict(newInput);
+```
+
+# 4. `MLContext` could be optional
+
+# 5. Setting options should just be properties on the Estimators
+
+Instead of defining a nested "Options" class for every type, we should just have properties on the Estimator classes. This way we don't need so many constructor overloads, and optional parameters. Instead, the constructor only takes the non-optional parameters, and the rest are properties on the type.
+
+_Note: Apache Spark has the same "Estimator/Transformer" concepts, and their Estimators are mutable._
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Runtime.Data;
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        public class IrisData
+        {
+            public float SepalLength { get; set; }
+            public float SepalWidth { get; set; }
+            public float PetalLength { get; set; }
+            public float PetalWidth { get; set; }
+            public string Label { get; set; }
+        }
+
+        public class IrisPrediction
+        {
+            public string PredictedLabel { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+
+            IDataView trainingData = mlContext.Data.ReadFromTextFile(
+                new[]
+                {
+                    new TextLoader.Column("SepalLength", DataKind.R4, 0),
+                    new TextLoader.Column("SepalWidth", DataKind.R4, 1),
+                    new TextLoader.Column("PetalLength", DataKind.R4, 2),
+                    new TextLoader.Column("PetalWidth", DataKind.R4, 3),
+                    new TextLoader.Column("Label", DataKind.Text, 4),
+                },
+                "iris-data.txt",
+                options => options.Separator = ",");
+
+            var estimator = mlContext.Transforms.Conversion.MapValueToKey("Label")
+                .Append(mlContext.Transforms.Concatenate("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent())
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+            var model = estimator.Fit(trainingData);
+
+            IrisData newInput = new IrisData()
+            {
+                SepalLength = 3.3f,
+                SepalWidth = 1.6f,
+                PetalLength = 0.2f,
+                PetalWidth = 5.1f,
+            };
+            IrisPrediction prediction = model
+                .MakePredictionFunction<IrisData, IrisPrediction>(mlContext)
+                .Predict(newInput);
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabel}");
+        }
+    }
+}
