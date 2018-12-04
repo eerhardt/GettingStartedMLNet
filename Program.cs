ce78567a8c5a9d122a036af935179ebb992b77eb
Dan Moseley <danmose@microsoft.com>
2018-12-03 17:54:21 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,33 +8,27 @@ using System;
 
 namespace myApp
 {
+    public class IrisData
+    {
+        public float SepalLength { get; set; }
+        public float SepalWidth { get; set; }
+        public float PetalLength { get; set; }
+        public float PetalWidth { get; set; }
+        public string Label { get; set; }
+    }
+
+    public class IrisPrediction
+    {
+        public string PredictedLabel { get; set; }
+    }
+
     class Program
     {
-        public class IrisData
-        {
-            [Column(0)]
-            public float SepalLength { get; set; }
-            [Column(1)]
-            public float SepalWidth { get; set; }
-            [Column(2)]
-            public float PetalLength { get; set; }
-            [Column(3)]
-            public float PetalWidth { get; set; }
-            [Column(4)]
-            public string Label { get; set; }
-        }
-
-        public class IrisPrediction
-        {
-            public string PredictedLabel { get; set; }
-        }
-
         static void Main(string[] args)
         {
-            TextLoader loader = TextLoader.Create(typeof(IrisData));
-            loader.Separator = ",";
+            var loader = new TextLoader(typeof(IrisData), separator = ",");
 
-            IDataView trainingData = loader.Read("iris-data.txt");
+            var trainingData = loader.Read("iris-data.txt");
 
             var estimator = new ValueToKeyMappingEstimator("Label")
                 .Append(new ColumnConcatenatingEstimator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
@@ -43,14 +37,15 @@ namespace myApp
 
             var model = estimator.Train<IrisData, IrisPrediction>(trainingData);
 
-            IrisData newInput = new IrisData()
+            var newInput = new IrisData()
             {
                 SepalLength = 3.3f,
                 SepalWidth = 1.6f,
                 PetalLength = 0.2f,
                 PetalWidth = 5.1f,
             };
-            IrisPrediction prediction = model.Predict(newInput);
+            
+            var prediction = model.Predict(newInput);
 
             Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabel}");
         }
